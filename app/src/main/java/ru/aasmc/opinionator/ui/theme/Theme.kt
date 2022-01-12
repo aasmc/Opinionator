fc4438bb329f310c9d44ecc546ba3bb2aaecbd96
Alex Alexeyev <aasmc@yandex.ru>
2022-01-12 20:34:03 +0300

    Initial Commit

---
+++ app/src/main/java/ru/aasmc/opinionator/ui/theme/Theme.kt
@@ -0,0 +1,39 @@
+package ru.aasmc.opinionator.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Color.White
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Color.LightGray
+)
+
+@Composable
+fun OpinionatorTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
